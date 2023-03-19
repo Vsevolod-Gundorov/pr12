5b8efe614d8634ea4adacf4eb3ad19c77bc5ffbf
unknown <seva.gundorov@mail.ru>
2023-03-19 23:39:30 +0300

    Add tests to GitHub Actions

--- tests/test_main.py
+++ tests/test_main.py
@@ -22,7 +22,6 @@ class TestDocuments():
         assert response.json().get('id') == 0
 
     def test_get_doc_by_id(self):
-        body = {"title": "New title", "body": "TEXT"}
         response = requests.post(f'{api_url}/v1/docs/0')
         assert response.status_code == 200
         assert response.json().get('title') == 'New title'
