a4e9a59d4762fdf3231e1d010584d954e8b2d3be
unknown <seva.gundorov@mail.ru>
2023-03-19 23:36:45 +0300

    Add tests to GitHub Actions

--- tests/test_main.py
+++ tests/test_main.py
@@ -17,17 +17,17 @@ class TestDocuments():
         body = {"title": "New title", "body": "TEXT"}
         response = requests.post(f'{api_url}/v1/docs', json=body)
         assert response.status_code == 200
-        assert response.json().gget('title') == 'New title'
-        assert response.json().gget('body') == 'TEXT'
-        assert response.json().gget('id') == 0
+        assert response.json().get('title') == 'New title'
+        assert response.json().get('body') == 'TEXT'
+        assert response.json().get('id') == 0
 
     def test_get_doc_by_id(self):
         body = {"title": "New title", "body": "TEXT"}
         response = requests.post(f'{api_url}/v1/docs/0')
         assert response.status_code == 200
-        assert response.json().gget('title') == 'New title'
-        assert response.json().gget('body') == 'TEXT'
-        assert response.json().gget('id') == 0
+        assert response.json().get('title') == 'New title'
+        assert response.json().get('body') == 'TEXT'
+        assert response.json().get('id') == 0
 
     def test_get_not_empty_docs(self):
         response = requests.get(f'{api_url}/v1/docs')
